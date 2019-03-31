7cd2ca7f821c15de42f82fb1e36cf6cc536311fe
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-03-31 01:43:14 +0100

    Write type annotations

--- zielbruks/settings.py
+++ zielbruks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from typing import List
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = 'hmw*j+x*54t2bjud*(tdfl$no@m5w$d%k*gbzi3cf^usdq87wj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTSL: List[str] = []
 
 
 # Application definition
