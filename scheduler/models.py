357447c08442d6564af525eff9832f14323c9b7d
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-03-31 20:57:36 +0200

    Add docstring and adjust pylintrc
    Disable warnings about mismatched overriding function signature.
    They were triggered by a catch-all override of django model `save()`
    method.

--- scheduler/models.py
+++ scheduler/models.py
@@ -4,19 +4,22 @@ from django.db import models
 # Create your models here.
 
 class Professor(models.Model):
+    """Person lecturing in a Lesson"""
     name = models.CharField("Professor name", max_length=100)
     surname = models.CharField("Professor surname", max_length=100)
-    email = models.EmailField("Professor email", max_length=100, null=True, blank=True, unique=True)#blank=not required
+    # blank=not required
+    email = models.EmailField("Professor email", max_length=100, null=True, blank=True, unique=True)
 
 
     def save(self, *args, **kwargs):
-        """Overriding save to run overrided clean()"""
+        """Overriding save to run overridden clean()"""
         self.full_clean()
         return super(Professor, self).save(*args, **kwargs)
 
     def clean(self):
         """Clean up blank fields to null.
-        Unless we give email, it will become "" and we aren't allowed to have more then one because of uniq
+        Unless we give email, it will become ""
+        and we aren't allowed to have more then one because of uniq
         """
         if self.email == "":
             self.email = None
@@ -25,19 +28,23 @@ class Professor(models.Model):
         return self.name + " " + self.surname
 
 class Auditorium(models.Model):
+    """Place at which a Lesson is given"""
     number = models.CharField("Auditorium number", max_length=30, unique=True)
 
     def __str__(self):
         return str(self.number)
 
 class Group(models.Model):
+    """Group of Students attending the same courses"""
     number = models.IntegerField("Group number", unique=True)
 
     def __str__(self):
         return str(self.number)
 
 class Lesson(models.Model):
-    """If you want to get a list of lessons from Foreign Tables, you should use related_name"""
+    """A university class having specific time and place"""
+
+    # If you want to get a list of lessons from Foreign Tables, you should use related_name
     name = models.CharField("Lesson name", max_length=100, unique=True)
     professor = models.ForeignKey(Professor, related_name='lessons', on_delete=models.CASCADE)
     auditorium = models.ForeignKey(Auditorium, related_name='lessons', on_delete=models.CASCADE)
@@ -49,6 +56,8 @@ class Lesson(models.Model):
         return self.name
 
 class Student(models.Model):
+    """Student particiapting in Lessons"""
+
     name = models.CharField("Student name", max_length=100)
     surname = models.CharField("Student surname", max_length=100)
     group = models.ForeignKey(Group, related_name='students', on_delete=models.SET_NULL, null=True)
