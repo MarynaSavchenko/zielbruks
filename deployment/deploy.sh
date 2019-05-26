f45104e6d8feb5a931d1a224e08da1d6339bcc0d
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-26 04:01:29 +0200

    Ensure proper chmod on private key

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 set -e
+PRIVKEY=deployment/deployment-push.key
 
 # make deployment ssh identity known to ssh
 ssh-keyscan -H $DEPLOYMENT_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
 
 # decrypt private ssh key used for deployment
-openssl aes-256-cbc -K $encrypted_cc51b64530dc_key -iv $encrypted_cc51b64530dc_iv -in deployment/deployment-push.enc -out deployment/deployment-push.key -d
+openssl aes-256-cbc -K $encrypted_cc51b64530dc_key -iv $encrypted_cc51b64530dc_iv -in deployment/deployment-push.enc -out "$PRIVKEY" -d
+chmod 600 "$PRIVKEY"
 
 
 ssh -i deployment/deployment-push.key $DEPLOYMENT_HOST $1
