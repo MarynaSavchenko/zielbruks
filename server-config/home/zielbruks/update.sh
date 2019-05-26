4d9be6711886382ae999baf617f224167175f401
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-26 02:35:30 +0200

    Add script for updating deployed application

---
+++ server-config/home/zielbruks/update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+SITE=$1
+
+if [[ "$SITE" != "staging" ]] && [[ "$SITE" != "production" ]]; then
+	echo "First argument must specify environment: production | staging"
+	exit 2
+fi
+
+PIP="$HOME/venv/$SITE/bin/pip"
+PYTHON="$HOME/venv/$SITE/bin/python"
+CODEDIR="$HOME/$SITE/zielbruks"
+
+cd "$CODEDIR";
+git fetch && git reset --hard '@{u}';
+"$PIP" install -r ./requirements.txt
+
+"$PYTHON" ./manage.py migrate
+
+# reload gunicorn
+pkill -SIGHUP gunicorn;
