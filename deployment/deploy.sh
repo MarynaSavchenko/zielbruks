e0134694a0a4b5939b590f208729fc4e50c70c66
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-26 03:53:55 +0200

    Do not use nonexistent variable

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 # make deployment ssh identity known to ssh
-ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H $DEPLOYMENT_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
+ssh-keyscan -H $DEPLOYMENT_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
 
 # decrypt private ssh key used for deployment
 openssl aes-256-cbc -K $encrypted_cc51b64530dc_key -iv $encrypted_cc51b64530dc_iv -in deployment/deployment-push.enc -out deployment/deployment-push.key -d
