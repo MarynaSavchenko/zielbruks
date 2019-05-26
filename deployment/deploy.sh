5a053df4e21e084867cba4dfecb6050fcac2e679
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-05-26 03:50:19 +0200

    Move ssh_known_host setup to script to have env variables available

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,4 +2,11 @@
 
 set -e
 
+# make deployment ssh identity known to ssh
+ssh-keyscan -t $TRAVIS_SSH_KEY_TYPES -H $DEPLOYMENT_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
+
+# decrypt private ssh key used for deployment
+openssl aes-256-cbc -K $encrypted_cc51b64530dc_key -iv $encrypted_cc51b64530dc_iv -in deployment/deployment-push.enc -out deployment/deployment-push.key -d
+
+
 ssh -i deployment/deployment-push.key $DEPLOYMENT_HOST $1
