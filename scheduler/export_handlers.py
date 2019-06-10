86a88ddb0cd213445a89f2b44cf5f201c71f80ca
Julia Dutkiewicz <dutkiewicz.julia@gmail.com>
2019-06-10 20:30:20 +0200

    Windows export bug fix

--- scheduler/export_handlers.py
+++ scheduler/export_handlers.py
@@ -8,14 +8,14 @@ from scheduler.models import Lesson, Group, Auditorium
 
 def export_to_csv(start_time, end_time):
     data_frame = get_lessons(start_time, end_time)
-    temp_file = NamedTemporaryFile(suffix='.csv')
+    temp_file = NamedTemporaryFile(suffix='.csv', delete=False)
     data_frame.to_csv(temp_file.name)
     return temp_file
 
 
 def export_to_excel(start_time, end_time):
     data_frame = get_lessons(start_time, end_time)
-    temp_file = NamedTemporaryFile(suffix='.xlsx')
+    temp_file = NamedTemporaryFile(suffix='.xlsx', delete=False)
     data_frame.to_csv(temp_file.name)
     return temp_file
 
