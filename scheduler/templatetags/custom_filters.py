b874556ccde66c7810d9b6876330578eafe31157
MarynaSavchenko <mvadimovna@ukr.net>
2019-06-11 00:44:35 +0200

    show new conflicts with red color on the conflict list

--- scheduler/templatetags/custom_filters.py
+++ scheduler/templatetags/custom_filters.py
@@ -3,9 +3,16 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag
 def get_color_tag(counter):
     """returns color tag based on parity of the counter"""
-    if counter%2 == 0:
+    if counter % 2 == 0:
         return 'warning'
     return 'info'
+
+
+@register.simple_tag
+def get_danger_tag():
+    """returns color tag 'danger'"""
+    return 'danger'
