907bfb5c274cc6da76cba81b958b3cb58901a3b9
kalfi44 <jkalfas179@gmial.com>
2019-06-12 20:44:53 +0200

    pylint&mypy changes

--- zielbruks/settings.py
+++ zielbruks/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 import os
-import djcelery
 from typing import List
+import djcelery
 from scheduler.task import notify_professors
 djcelery.setup_loader()
 
