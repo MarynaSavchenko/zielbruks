b2c46f117a9a23acf01bc9c4e4f4b2024f4fec1d
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-06-13 02:15:01 +0200

    Fix name of celery configuration variable

--- zielbruks/settings.py
+++ zielbruks/settings.py
@@ -148,4 +148,4 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
-CELERY_BEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
+CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
